a5b58011c604d6f9e772b892bb72e8f0a8e132c7
lucasgabrielnobre <lucasgabrielprofs@gmail.com>
2026-06-16 10:07:53 -0300

    extensoes

--- lerAcorde.py
+++ lerAcorde.py
@@ -1,44 +1,45 @@
 from notaValor import *
-#dado um s retornar os valores 
-maior = [0, 0, 2, 4, 5, 7, 9, 11] # b5 maior[5] - 1
-menor = [0, 0, 2, 3, 5, 7, 8, 10]
+maior     = [0, 0, 2, 4, 5, 7, 9, 11] # b5 maior[5] - 1
+mixolidio = [0, 0, 2, 4, 5, 7, 9, 10] 
+menor     = [0, 0, 2, 3, 5, 7, 8, 10]
 diminuta  = [0, 0, 1, 3, 5, 6, 8, 10] # Lócrio (contém 1, b3, b5)
 aumentada = [0, 0, 2, 4, 6, 8, 9, 11] # Lídio Aumentado (contém 1, 3, #5)
 
-def add(t, n):
-    return (t + n) % 12
+def jump(t, x):
+    return (t + x) % 12
 
 def lerAcorde(s):
-    # aug, +     => 0, 4, 8        -> 1 3 5#
-    # dim, -     => 0, 3, 6, (10)  -> 1 3b 5b
-    # sus2       => 0, 2, 7        -> 1 2 5
-    # sus4       => 0, 2, 5        -> 1 4 5
-    acorde = {'tonica' : 0, 'qualidade' : "maior", 'extensoes': [], 'triade' : [], 'baixo' : 0 }
+    acorde = {'tonica' : 0, 'qualidade' : "mixolidio", 'extensoes': [], 'triade' : [], 'baixo' : 0 }
     #tonica
     if  len(s) >= 2 and (s[1] == 'b' or s[1] == '#'):
         acorde['tonica'] = notaValor[s[0] + s[1]]
     else:
         acorde['tonica'] = notaValor[s[0]]
     t = acorde['tonica']
-
+    acorde['baixo'] = t
     #extensoes
-    s_ext = []
-    doubleDigit = False
-    for i in range(len(s)): 
-        if s[i].isdigit():
-            if doubleDigit:
-                s_ext[i - 1] += s[i]
-                doubleDigit = False
-            if s[i] == '1':
-                doubleDigit = True
-    print(s_ext)
+    add = '' 
+    for i in range(len(s)):
+        c = s[i]
+        if c == 'b' or c == '#':
+            add = c
+        if c.isdigit():
+            if c != '1' or (add and add[-1] == '1'):
+                c = add + c
+                acorde['extensoes'].append(c)
+                add = ''
+            else:
+                add += c
+            
 
     #qualidade
-    if "m" in s and not ("maj" in s):
+    if "m" in s:
         acorde['qualidade'] = "menor"
+    if "maj" in s or "M" in s: # se for maj vai sobrepor, se não fica menor
+        acorde['qualidade'] = "maior"
     if "aug" in s:
         acorde['qualidade'] = "aumentada"
-    if "dim" in s: 
+    if "dim" in s: #idem
         acorde['qualidade'] = "diminuta"
     if "sus2" in s: 
         acorde['qualidade'] = "sus2"
@@ -47,33 +48,86 @@ def lerAcorde(s):
     # baixo
     if '/' in s[-3:-1]:
         if s[-1] == 'b' or s[-1] == '#':
-            acorde['baixo'] = notaValor[s[-2] + s[-1]]
+            acorde['baixo'] = s[-2] + s[-1]
         else:
-            acorde['baixo'] = notaValor[s[-1]]
+            acorde['baixo'] = s[-1]
     else:
         acorde['baixo'] = t
     # colocando a triade
-    if acorde['qualidade'] == "maior":
-        acorde['triade'] = [valorNota[t][0], valorNota[add(t, maior[3])][0], valorNota[add(t, maior[5])][0]]
+    if acorde['qualidade'] == "maior" or acorde['qualidade'] == "mixolidio":
+        acorde['triade'] = [valorNota[t][0], valorNota[jump(t, maior[3])][0], valorNota[jump(t, maior[5])][0]]
     elif acorde['qualidade'] == "menor":
-        acorde['triade'] = [valorNota[t][0], valorNota[add(t, menor[3])][-1], valorNota[add(t, menor[5])][0]]
-        acorde['triade'] = [t, add(t, menor[3]), add(t, menor[5])]
+        acorde['triade'] = [valorNota[t][0], valorNota[jump(t, menor[3])][-1], valorNota[jump(t, menor[5])][0]]
     elif acorde['qualidade'] == "aumentada":
-        acorde['triade'] = [valorNota[t][0], valorNota[add(t, aumentada[3])][0], valorNota[add(t, aumentada[5])][0]]
+        acorde['triade'] = [valorNota[t][0], valorNota[jump(t, aumentada[3])][0], valorNota[jump(t, aumentada[5])][0]]
     elif acorde['qualidade'] == "diminuta":
-        acorde['triade'] = [valorNota[t][0], valorNota[add(t, diminuta[3])][-1], valorNota[add(t, diminuta[5])][-1]]
+        acorde['triade'] = [valorNota[t][0], valorNota[jump(t, diminuta[3])][-1], valorNota[jump(t, diminuta[5])][-1]]
     elif acorde['qualidade'] == "sus2":
-        acorde['triade'] = [valorNota[t][0], valorNota[add(t, maior[2])][0], valorNota[add(t, maior[5])][0]]
+        acorde['triade'] = [valorNota[t][0], valorNota[jump(t, maior[2])][0], valorNota[jump(t, maior[5])][0]]
     elif acorde['qualidade'] == "sus4":
-        acorde['triade'] = [valorNota[t][0], valorNota[add(t, maior[4])][0], valorNota[add(t, maior[5])][0]]
-
-    
+        acorde['triade'] = [valorNota[t][0], valorNota[jump(t, maior[4])][0], valorNota[jump(t, maior[5])][0]]
     return acorde
 
 def notasAcorde(s):
     # notas + notaValor
     acorde = lerAcorde(s)
-    notas = acorde['triade']
+    notas = []
+    t = acorde['tonica']
+    q = acorde['qualidade']
+    if (t != acorde['baixo']): 
+        notas.append(acorde['baixo'])
+    notas += acorde['triade']
+    if len(acorde['extensoes']) > 0: # se tem extensao tem a setima C9 => C E G Bb D
+        if q == "maior":
+            notas.append(valorNota[jump(t, maior[7])][0])
+        else:
+            notas.append(valorNota[jump(t, menor[7])][-1])
+    for ext in acorde['extensoes']:
+        # 4 casos: 5, b5, 13, b13
+        grau = 0
+        acidente = ''
+        if (len(ext) == 1):
+            grau = int(ext)
+        elif (len(ext) == 2):
+            if ext[0].isnumeric():
+                grau = int(ext)
+            else:
+                grau = int(ext[1])
+                acidente = ext[0]
+        else:
+            acidente = ext[0]
+            grau =  int(ext[1:3])
+        if grau >= 8:
+            grau -= 7
+        if grau == 7:
+            continue
+
+        valor = 0
+
+        if q == "maior":
+            valor = maior[grau]
+        elif q == "mixolidio":
+            valor = mixolidio[grau]
+        elif q == "menor":
+            valor = menor[grau]
+        elif q == "aumentada":
+            valor = aumentada[grau]
+        elif q == "diminuta":
+            valor = diminuta[grau]
+
+        if acidente == "b":
+            valor -= 1
+        elif acidente == "#":
+            valor += 1
+        #nota = valorNota[jump(t, valor)][1]+"/"+valorNota[jump(t,valor)][0] if len(valorNota[jump(t, valor)]) > 1 else valorNota[jump(t,valor)][-1] 
+        #nota = valorNota[ jump(t, valor) ][-1] if acidente == "b" else valorNota[ jump(t, valor) ][0] 
+        if acidente == "b":
+            nota = valorNota[ jump(t, valor) ][-1]
+        elif len(acorde['triade'][0]) > 1 and acorde['triade'][0][-1] == "b": # Db
+            nota = valorNota[ jump(t, valor) ][-1]
+        else:
+            nota = valorNota[ jump(t, valor) ][0]
+        notas.append(nota)
     return notas
 
 def main():
